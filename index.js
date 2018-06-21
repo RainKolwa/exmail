b422e254ebbf94cf2c033d8ba307bc541b97844c
rainkolwa <pluvo.lee@gmail.com>
2018-06-21 12:39:59 +0800

    用户删除

--- index.js
+++ index.js
@@ -3,24 +3,77 @@ import axios from "axios";
 import config from "./config";
 import api from "./api";
 import User from "./user";
+import store from "./store";
 
 const { corpId, corpSecret } = config;
 
+const MENU = [
+  {
+    type: "select",
+    name: "menu",
+    message: "What can I do for you?",
+    choices: [
+      { title: "User management", value: "1" },
+      { title: "Department management", value: "2" }
+    ]
+  },
+  {
+    type: "select",
+    name: "submenu",
+    message: "And?",
+    choices: prev =>
+      prev === "1"
+        ? [
+            { title: "Create user", value: "11" },
+            { title: "Delete user", value: "12" }
+          ]
+        : [
+            { title: "Create department", value: "21" },
+            { title: "Delete department", value: "22" }
+          ]
+  }
+];
+
 const getToken = async () => {
+  console.log("连接中...");
   const { data } = await api({
     method: "get",
     url: `gettoken?corpid=${corpId}&corpsecret=${corpSecret}`
   });
-  console.log("token", data.access_token);
-  return data.access_token;
+  if (data.errcode) {
+    console.log("连接失败");
+    console.log(data.errmsg);
+    return false;
+  } else {
+    console.log("连接成功");
+    store.accessToken = data.access_token;
+    return data.access_token;
+  }
 };
 
 const app = async () => {
   const token = await getToken();
 
-  // create user
-  const user = new User();
-  user.create(token);
+  if (!token) return;
+
+  // 显示主菜单
+  const res = await prompts(MENU);
+  let user;
+  const { menu, submenu } = res || {};
+  if (menu === "1") {
+    user = new User();
+  }
+
+  switch (submenu) {
+    case "11":
+      user.create();
+      return false;
+    case "12":
+      user.delete();
+      return false;
+    default:
+      return false;
+  }
 };
 
 app();
--- user.js
+++ user.js
@@ -1,6 +1,8 @@
 import axios from "axios";
+import prompts from "prompts";
 import api from "./api";
 import config from "./config";
+import store from "./store";
 
 const { domain, defaultPassword } = config;
 
@@ -29,7 +31,18 @@ const questions = [
 ];
 
 class User {
-  async create({ token }) {
+  async list() {
+    const res = await api({
+      method: "get",
+      url: `user/simplelist?access_token=${
+        store.accessToken
+      }&department_id=1&fetch_child=1`
+    });
+
+    return res.data.userlist;
+  }
+
+  async create() {
     const response = await prompts(questions);
     const params = {
       department: [1],
@@ -46,7 +59,7 @@ class User {
     console.log("creating user...");
     const res = await api({
       method: "post",
-      url: `user/create?access_token=${token}`,
+      url: `user/create?access_token=${store.accessToken}`,
       data: params
     });
     // Done
@@ -54,6 +67,40 @@ class User {
       console.log("Successfully created");
     } else {
       console.log(res.data);
+    }
+  }
+
+  async delete() {
+    const users = await this.list();
+    const questions = users.map(user => ({
+      title: `${user.name}-${user.userid}`,
+      value: user.userid
+    }));
+    const form = await prompts([
+      {
+        type: "select",
+        name: "user",
+        message: "Select a user",
+        choices: questions
+      },
+      {
+        type: "select",
+        name: "confirm",
+        message: "Are you sure to delete?",
+        choices: [{ title: "Yes", value: 1 }, { title: "No", value: 0 }]
+      }
+    ]);
+    if (form.user && form.confirm) {
+      // excute deletion
+      const res = await api({
+        method: "get",
+        url: `user/delete?access_token=${store.accessToken}&userid=${form.user}`
+      });
+      if (res.data.errcode === 0) {
+        console.log("Successfully deleted");
+      } else {
+        console.log(res.data.errmsg);
+      }
     }
   }
 }
